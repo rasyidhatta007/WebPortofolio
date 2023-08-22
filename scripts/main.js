d17135b6962209aa153871a48831d3cfcfbd0d20
rasyidhatta007 <rasyidhatta007@gmail.com>
2023-08-22 16:08:56 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+//togle & Responsive Navigation 
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+//clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName ("form")) {
+        form.reset ();
+    }
+};
+
