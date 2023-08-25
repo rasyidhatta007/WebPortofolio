349fa88867776061a75c3d0d7f2dbc0a35ccdf06
rasyidhatta007 <rasyidhatta007@gmail.com>
2023-08-26 00:23:07 +0700

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,35 @@ window.onbeforeunload = () => {
     }
 };
 
+// JavaScript for Lightbox
+const openLightbox = document.getElementById('openLightbox');
+const portofolioLinks = document.querySelectorAll('.portofolio-link');
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox img');
+
+portofolioLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const portofolioCover = this.closest('.portofolio').querySelector('.portofolio-cover img');
+        lightboxImage.src = portofolioCover.src;
+        lightbox.style.display = 'flex';
+    });
+});
+
+lightbox.addEventListener('click', function(event) {
+    if (event.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
+});
+
+
+openLightbox.addEventListener('click', function(event) {
+    event.preventDefault();
+    lightboxImage.src = this.querySelector('img').src;
+    lightbox.style.display = 'flex';
+});
+
+lightbox.addEventListener('click', function(event) {
+    lightbox.style.display = 'none';
+    history.back(); // Navigasi mundur ke halaman sebelumnya
+});
